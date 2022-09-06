8642d1fd4e172f38f457a64b9229cfd0c42f5008
SaranRJ <saranrj1498@gmail.com>
2022-09-06 15:45:55 +0530

    Saranrj's code

--- src/main/java/org/sel/BaseClass.java
+++ src/main/java/org/sel/BaseClass.java
@@ -2,4 +2,10 @@ package org.sel;
 
 public class BaseClass {
 
+	public static void main(String[] args) {
+		System.out.println("Saran");
+		System.out.println("rj");
+		System.out.println("Saran R");
+		
+	}
 }
